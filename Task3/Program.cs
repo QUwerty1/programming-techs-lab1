8e2d958509fd8ea0e4edc6d475de9a320bdaee23
QUwerty1 <mrquwerty@mail.ru>
2025-02-12 23:57:34 +0800

    Задание 3 сделано

---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿static class Program
+{
+    internal static void Main()
+    {
+        var text = "1+4-3+2";
+        Console.WriteLine(Solver.FindSumFromText(text));
+    }
+}
+
+static class Solver
+{
+    /// <summary>
+    /// Находит сумму чисел в строке
+    /// </summary>
+    /// <param name="text">Строка в формате d1±d2±...±dn, где di > 0</param>
+    /// <returns>Сумма чисел</returns>
+    public static int FindSumFromText(string text)
+    {
+        int numStartPos = 0;
+        int sum = 0;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if ((numStartPos != i) && (text[i] is '-' or '+'))
+            {
+                sum += Convert.ToInt32(text[numStartPos..i]);
+                numStartPos = i;
+            }
+        }
+        sum += Convert.ToInt32(text[numStartPos..]);
+
+        return sum;
+    }
+}
