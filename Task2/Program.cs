aaa4f306cbfbaec932e16413117502232853519e
QUwerty1 <mrquwerty@mail.ru>
2025-02-12 23:30:10 +0800

    Сделано второе задание и тесты для него

---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    internal static void Main()
+    {
+        var ids = Solver.FindPair([.. Console.ReadLine()!.Split(' ').Select(n => Convert.ToInt32(n))]);
+        if (ids is null)
+        {
+            Console.WriteLine("Пар не обнаружено");
+        }
+        else
+        {
+            Console.WriteLine(
+                $"Id первой пары одинаковых чисел {ids.Value.Item1} и {ids.Value.Item2}");
+        }
+    }
+}
+
+public class Solver
+{
+    /// <summary>
+    /// Возвращает id-шники первой пары соседних одинаковых элементов или null, если таких не обнаружено
+    /// </summary>
+    /// <param name="array">Массив чисел</param>
+    /// <returns>Пара id-шников</returns>
+    public static (int, int)? FindPair(int[] array)
+    {
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i - 1] == array[i])
+                return (i - 1, i);
+        }
+
+        return null;
+    }
+}
---
+++ TestProject2/Test1.cs
@@ -0,0 +1,25 @@
+﻿namespace Task2.Test;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void PairExists()
+    {
+        int[] array = [1, 2, 3, 6, 3, 5, 2, 2, 3];
+        var pair = (6, 7);
+        Assert.AreEqual(Solver.FindPair(array)!, pair);
+    }
+    [TestMethod]
+    public void PairDoesNotExists()
+    {
+        int[] array = [1, 2, 6, 34, 58, 2, 23];
+        Assert.IsNull(Solver.FindPair(array));
+    }
+    [TestMethod]
+    public void NotNeighborEquivalentNumbers()
+    {
+        int[] array = [1, 2, 3, 6, 3, 5, 2, 3];
+        Assert.IsNull(Solver.FindPair(array));
+    }
+}
